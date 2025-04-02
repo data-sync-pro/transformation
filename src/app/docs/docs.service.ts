973141831e4788f413d3d3da4dc967026ee54908
Xuehang Xu <Xuehang.Xu@Pushtopic.com>
2025-04-02 18:57:38 -0400

    Format

--- src/app/docs/docs.service.ts
+++ src/app/docs/docs.service.ts
@@ -1,41 +1,38 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-
 interface Parameter {
-    name: string;
-    type: string;
-    required: string;
-    description: string;
-  }
+  name: string;
+  type: string;
+  required: string;
+  description: string;
+}
 
 export interface DocData {
-    title: string;
-    description: string;
-    syntax: string;
-    parameters?: Parameter[];
-    examples?: string[];
-    tips?: string[];
-    relatedFormulas?: string[];
+  title: string;
+  description: string;
+  syntax: string;
+  parameters?: Parameter[];
+  examples?: string[];
+  tips?: string[];
+  relatedFormulas?: string[];
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DocsService {
-    constructor(private http: HttpClient) {}
-  
-  
-    getDocByName(docName: string): Observable<DocData | null> {
-      const url = `assets/functions/${docName}.json`;
-      return this.http.get<DocData>(url).pipe(
-        catchError(error => {
-          console.error(`Error loading ${url}:`, error);
-          return of(null);
-        })
-      );
-    }
+  constructor(private http: HttpClient) {}
+
+  getDocByName(docName: string): Observable<DocData | null> {
+    const url = `assets/functions/${docName}.json`;
+    return this.http.get<DocData>(url).pipe(
+      catchError((error) => {
+        console.error(`Error loading ${url}:`, error);
+        return of(null);
+      })
+    );
   }
+}
